b5400b1b5e9621a60c75f22c4271820668ea538a
Thiago Leite <thiagofmleite@gmail.com>
2024-04-03 10:12:27 -0300

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,74 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Button from "./components/Button";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increase":
+      return state + 1;
+    case "decrease":
+      return state - 1;
+    case "reset":
+      return 0;
+    case "multiply":
+      return state * action.payload;
+    case "divide":
+      return state / action.payload;
+    default:
+      return state;
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
+  function increase() {
+    dispatch({ type: "increase" });
+  }
+
+  function decrease() {
+    dispatch({ type: "decrease" });
+  }
+
   return (
     <>
       <Header />
-      <div>
-        <form onSubmit={(event) => event.preventDefault()}>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" />
-          <Button width={300} margin={"2rem"}>
-            Submit
-          </Button>
-        </form>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "50%",
+          margin: "0 auto",
+        }}
+      >
+        <button onClick={() => dispatch({ type: "divide", payload: 2 })}>
+          --
+        </button>
+        <button onClick={() => decrease()}>-</button>
+        <pre
+          style={{
+            fontFamily: "monospace",
+            backgroundColor: "#ccc",
+            padding: "1rem",
+            width: "100px",
+            textAlign: "center",
+          }}
+        >
+          {state}
+        </pre>
+
+        <button onClick={() => increase()}>+</button>
+        <button onClick={() => dispatch({ type: "multiply", payload: 10 })}>
+          ++
+        </button>
+      </div>
+      <div style={{ margin: "1rem auto 0", width: "50%" }}>
+        <button
+          style={{ width: "100%" }}
+          onClick={() => dispatch({ type: "reset" })}
+        >
+          Reset
+        </button>
       </div>
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
+import React from "react";
+
 const Header = ({ children }) => {
+  console.log("Header rendered");
+
   return <header>{children || <h1>My React App</h1>}</header>;
 };
 
-export default Header;
+export default React.memo(Header);
